0f37d6f393ce60f8140dd605b79c8b259b6dcbe5
Teo Koon Peng <koonpeng@openrobotics.org>
2022-09-16 15:09:33 +0800

    fix missing joint names
    Signed-off-by: Teo Koon Peng <koonpeng@openrobotics.org>

--- src/rws_interface.cpp
+++ src/rws_interface.cpp
@@ -145,7 +145,15 @@ std::vector<cfg::moc::Joint> RWSInterface::getCFGJoints()
 
   for(size_t i = 0; i < instances.size(); ++i)
   {
-    std::vector<Poco::XML::Node*> attributes = xmlFindNodes(instances[i], XMLAttributes::CLASS_CFG_IA_T_LI);
+    std::vector<Poco::XML::Node*> attributes;
+    if (this->rws_client_.rws_version() >= 7)
+    {
+      attributes = xmlFindNodes(instances[i], XMLAttribute{"class", "cfg-ia-t"});
+    }
+    else
+    {
+      attributes = xmlFindNodes(instances[i], XMLAttributes::CLASS_CFG_IA_T_LI);
+    }
 
     cfg::moc::Joint joint;
 
